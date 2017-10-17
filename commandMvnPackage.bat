c285835a00b3a3452cc5c72dcacdcb89514ee9d1
chandra shekhar <chchandu123@gmail.com>
2017-10-17 18:00:53 +0530

    push all files

---
+++ commandMvnPackage.bat
@@ -0,0 +1,2 @@
+cd C:\Users\chanagondac\workspace\Indigo 
+mvn package
---
+++ src/main/java/ex/HelloWorld.java
@@ -0,0 +1,16 @@
+package ex;
+
+public class HelloWorld 
+{
+
+	public static void main(String args[]){
+
+		System.out.println("Hello World, Maven");
+		System.out.println("line 1");
+		System.out.println("line 222 ");
+		System.out.println("Line 3333");
+		System.out.println("October 11 7:16 pm");
+
+	}
+
+}
